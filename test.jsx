b8f50dc995aabb6d5fd42b072f5f6ffd13bf1ca6
VINSEVINALON\vinsevinalon <vinsevinalon@gmail.com>
2017-06-29 18:25:33 +0800

    Added bh-mj-header-divider

---
+++ test.jsx
@@ -0,0 +1,9 @@
+ 
+"use strict"; 
+ 
+import React from "react"; 
+import ReactDOM from "react-dom"; 
+ 
+import HeaderDivider from "./header-divider.js"; 
+ 
+ReactDOM.render( <HeaderDivider></HeaderDivider> ); 
