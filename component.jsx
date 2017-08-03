1bd4ae67841d04e5354a0618d1e9bb7fe14cc84c
VINSEVINALON\vinsevinalon <vinsevinalon@gmail.com>
2017-08-03 17:29:44 +0800

    Update bh-mj-header-divider component.

---
+++ component.jsx
@@ -0,0 +1,14 @@
+ 
+"use strict"; 
+ 
+import React from "react"; 
+import ReactDOM from "react-dom"; 
+ 
+import HeaderDivider from "./header-divider.js"; 
+ 
+ReactDOM.render( 
+	<HeaderDivider
+		label="This is a header.">
+	</HeaderDivider>,
+	document.getElementById( "root" ) 
+); 
--- header-divider.js
+++ header-divider.js
@@ -1,2 +1,122 @@
-"use strict";function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj}}Object.defineProperty(exports,"__esModule",{value:!0});var _extends2=require("babel-runtime/helpers/extends"),_extends3=_interopRequireDefault(_extends2),_getPrototypeOf=require("babel-runtime/core-js/object/get-prototype-of"),_getPrototypeOf2=_interopRequireDefault(_getPrototypeOf),_classCallCheck2=require("babel-runtime/helpers/classCallCheck"),_classCallCheck3=_interopRequireDefault(_classCallCheck2),_createClass2=require("babel-runtime/helpers/createClass"),_createClass3=_interopRequireDefault(_createClass2),_possibleConstructorReturn2=require("babel-runtime/helpers/possibleConstructorReturn"),_possibleConstructorReturn3=_interopRequireDefault(_possibleConstructorReturn2),_inherits2=require("babel-runtime/helpers/inherits"),_inherits3=_interopRequireDefault(_inherits2),_class,_mjmlCore=require("mjml-core"),_react=require("react"),_react2=_interopRequireDefault(_react),_mjmlColumn=require("mjml-column"),_mjmlColumn2=_interopRequireDefault(_mjmlColumn),_mjmlDivider=require("mjml-divider"),_mjmlDivider2=_interopRequireDefault(_mjmlDivider),_mjmlSection=require("mjml-section"),_mjmlSection2=_interopRequireDefault(_mjmlSection),_mjmlText=require("mjml-text"),_mjmlText2=_interopRequireDefault(_mjmlText),_wichevr=require("wichevr"),_wichevr2=_interopRequireDefault(_wichevr),tagName="mj-header-divider",parentTag=["mj-container"],endingTag=!1,defaultMJMLDefinition={content:"",attributes:{label:"Label","value-color":"black"}},HeaderDivider=(0,_mjmlCore.MJMLElement)(_class=function(_Component){function HeaderDivider(){return(0,_classCallCheck3.default)(this,HeaderDivider),(0,_possibleConstructorReturn3.default)(this,(HeaderDivider.__proto__||(0,_getPrototypeOf2.default)(HeaderDivider)).apply(this,arguments))}return(0,_inherits3.default)(HeaderDivider,_Component),(0,_createClass3.default)(HeaderDivider,[{key:"render",value:function render(){var mjAttribute=this.props.mjAttribute;return _react2.default.createElement(_mjmlSection2.default,(0,_extends3.default)({},this.props,{padding:"0px 0px 0px 0px","background-color":mjAttribute("value-color")}),_react2.default.createElement(_mjmlColumn2.default,{width:"70%","background-color":mjAttribute("label-color")},_react2.default.createElement(_mjmlText2.default,{"font-size":"16px","font-weight":"500","letter-spacing":"0.3px"},(0,_wichevr2.default)(mjAttribute("title"),mjAttribute("label")))),_react2.default.createElement(_mjmlDivider2.default,{"background-color":mjAttribute("value-color")}))}}]),HeaderDivider}(_react.Component))||_class;HeaderDivider.tagName=tagName,HeaderDivider.parentTag=parentTag,HeaderDivider.endingTag=endingTag,HeaderDivider.defaultMJMLDefinition=defaultMJMLDefinition,exports.default=HeaderDivider;
+"use strict";
+
+/*;
+                @module-license:
+              		The MIT License (MIT)
+              		@mit-license
+              
+              		Copyright (@c) 2017 Biyaheroes Developers
+              		@email: developers@biyaheroes.com
+              
+              		Permission is hereby granted, free of charge, to any person obtaining a copy
+              		of this software and associated documentation files (the "Software"), to deal
+              		in the Software without restriction, including without limitation the rights
+              		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+              		copies of the Software, and to permit persons to whom the Software is
+              		furnished to do so, subject to the following conditions:
+              
+              		The above copyright notice and this permission notice shall be included in all
+              		copies or substantial portions of the Software.
+              
+              		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+              		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+              		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+              		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+              		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+              		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+              		SOFTWARE.
+                @end-module-license
+              
+                @module-configuration:
+              	{
+              	  "package": "bh-mj-header-divider",
+              	  "path": "bh-mj-header-divider/header-divider.jsx",
+              	  "file": "header-divider.jsx",
+              	  "module": "header-divider",
+              	  "author": "Biyaheroes Developers",
+              	  "contributors": [
+              		"Robot Biyaheroes <robot@biyaheroes.com>",
+              		"Vinse Vinalon <vinsevinalon@gmail.com>"
+              	  ],
+              	  "eMail": "developers@biyaheroes.com",
+              	  "repository": "https://github.com/Biyaheroes/bh-mj-detail.git",
+              	  "global": true
+              	}
+                @end-module-configuration
+              
+                @module-documentation:
+              	Biyaheroes MJML Focus Detail Component
+                @end-module-documentation
+              
+                @include:
+              	{
+              	  "MJMLElement": "mjml-core",
+              	  "React": "react",
+              	  "Component": "react.Component",
+              	  "Column": "mjml-column",
+              	  "Table": "mjml-table",
+              	  "wichevr": "wichevr"
+              	}
+                @end-include
+              */Object.defineProperty(exports, "__esModule", { value: true });var _extends2 = require("babel-runtime/helpers/extends");var _extends3 = _interopRequireDefault(_extends2);var _getPrototypeOf = require("babel-runtime/core-js/object/get-prototype-of");var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);var _classCallCheck2 = require("babel-runtime/helpers/classCallCheck");var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);var _createClass2 = require("babel-runtime/helpers/createClass");var _createClass3 = _interopRequireDefault(_createClass2);var _possibleConstructorReturn2 = require("babel-runtime/helpers/possibleConstructorReturn");var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);var _inherits2 = require("babel-runtime/helpers/inherits");var _inherits3 = _interopRequireDefault(_inherits2);var _class;
+
+var _mjmlCore = require("mjml-core");
+var _react = require("react");var _react2 = _interopRequireDefault(_react);
+var _mjmlColumn = require("mjml-column");var _mjmlColumn2 = _interopRequireDefault(_mjmlColumn);
+var _mjmlDivider = require("mjml-divider");var _mjmlDivider2 = _interopRequireDefault(_mjmlDivider);
+var _mjmlSection = require("mjml-section");var _mjmlSection2 = _interopRequireDefault(_mjmlSection);
+var _mjmlText = require("mjml-text");var _mjmlText2 = _interopRequireDefault(_mjmlText);
+var _wichevr = require("wichevr");var _wichevr2 = _interopRequireDefault(_wichevr);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
+
+var tagName = "mj-header-divider";
+
+var parentTag = ["mj-container"];
+
+var endingTag = false;
+
+var defaultMJMLDefinition = {
+	"content": "",
+	"attributes": {
+		"label": "Label",
+		"value-color": "black" } };var
+
+
+
+
+HeaderDivider = (0, _mjmlCore.MJMLElement)(_class = function (_Component) {(0, _inherits3.default)(HeaderDivider, _Component);function HeaderDivider() {(0, _classCallCheck3.default)(this, HeaderDivider);return (0, _possibleConstructorReturn3.default)(this, (HeaderDivider.__proto__ || (0, _getPrototypeOf2.default)(HeaderDivider)).apply(this, arguments));}(0, _createClass3.default)(HeaderDivider, [{ key: "render", value: function render()
+		{var
+			mjAttribute = this.props.mjAttribute;
+
+			return (
+				_react2.default.createElement(_mjmlSection2.default, (0, _extends3.default)({},
+					this.props, {
+						padding: "0px 0px 0px 0px",
+						"background-color": mjAttribute("value-color") }),
+
+					_react2.default.createElement(_mjmlColumn2.default, {
+							width: "70%",
+							"background-color": mjAttribute("label-color") },
+
+						_react2.default.createElement(_mjmlText2.default, {
+								"font-size": "16px",
+								"font-weight": "500",
+								"letter-spacing": "0.3px" },
+							(0, _wichevr2.default)(mjAttribute("title"), mjAttribute("label")))),
+
+
+					_react2.default.createElement(_mjmlDivider2.default, {
+						"background-color": mjAttribute("value-color") })));
+
+
+
+		} }]);return HeaderDivider;}(_react.Component)) || _class;
+
+
+HeaderDivider.tagName = tagName;
+HeaderDivider.parentTag = parentTag;
+HeaderDivider.endingTag = endingTag;
+HeaderDivider.defaultMJMLDefinition = defaultMJMLDefinition;exports.default =
+
+HeaderDivider;
+
 //# sourceMappingURL=header-divider.js.map
